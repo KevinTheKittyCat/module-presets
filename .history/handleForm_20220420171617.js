a16f8a82ba9217243e3bcd4eb2227a878e500678
Kevin <kevijens@hotmail.com>
2022-05-14 11:07:09 +0200

    delete history

--- .history/handleForm_20220420171617.js
+++
@@ -1,5 +0,0 @@
-function myFunc() {
-    var formText = $("#textInput").val();
-    // Do something with formText
-    console.log(formText)
-  }
--- .history/unpause_20210816214514.js
+++
@@ -1,6 +0,0 @@
-Hooks.once('ready', () => {
-    if (game.paused) {
-        game.togglePause(false, true);
-		console.log("unpaused")
-    }
-})
